98cdf3980034bd4ed1e1d3f69d66b636cc9b0a4b
Melvin Tehubijuluw <melvin.tehubijuluw@beeproger.com>
2017-02-19 16:31:42 +0100

    Ik commit weer terug, ja is goed!

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -176,10 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Snowfire\Beautymail\BeautymailServiceProvider::class,
-
-         Intervention\Image\ImageServiceProvider::class,
 
 
     ],
@@ -231,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
