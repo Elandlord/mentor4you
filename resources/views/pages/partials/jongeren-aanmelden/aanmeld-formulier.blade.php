f7a23502c522b31d4131165e1e9b16dd7dbbee5b
Eric Landheer <Eric Landheer>
2017-02-20 09:50:20 +0100

    Label aanmeldformulieren

--- resources/views/pages/partials/jongeren-aanmelden/aanmeld-formulier.blade.php
+++ resources/views/pages/partials/jongeren-aanmelden/aanmeld-formulier.blade.php
@@ -9,36 +9,36 @@
         <div class="col-lg-12 space-outside-down-md">
           <div class="row">
             <div class="col-lg-5 space-outside-up-sm">
-              <label>Voornaam</label>
-              <input type='text' name='voornaam' placeholder='Voornaam' class='space-inside-xs space-inside-sides-sm
+              <label class='font-md text-light xs-text-center'>Voornaam</label>
+              <input type='text' name='voornaam' placeholder='Voornaam' class='space-inside-xs font-md space-inside-sides-sm
           font-sm xs-text-center height-auto border border-accent space-outside-xs' required />
             </div>
             <div class="col-lg-5 clear-left space-outside-up-sm">
-              <label>Achternaam</label>
+              <label class='font-md text-light xs-text-center'>Achternaam</label>
               <input type='text' name='achternaam' placeholder='Achternaam' class='space-inside-xs space-inside-sides-sm
           font-sm xs-text-center height-auto border border-accent space-outside-xs' required />
             </div>
             <div class="col-lg-5 clear-left space-outside-up-sm">
-              <label>Telefoonnummer</label>
+              <label class='font-md text-light xs-text-center'>Telefoonnummer</label>
               <input type='text' name='telefoonnummer' placeholder='Telefoonnummer' class='space-inside-xs space-inside-sides-sm
           font-sm xs-text-center height-auto border border-accent space-outside-xs' required />
             </div>
             <div class="col-lg-5 clear-left space-outside-up-sm">
-              <label>Emailadres</label>
+              <label class='font-md text-light xs-text-center'>Emailadres</label>
               <input type='text' name='emailadres' placeholder='Emailadres' class='space-inside-xs space-inside-sides-sm
           font-sm xs-text-center height-auto border border-accent space-outside-xs' required />
             </div>
             <div class="col-lg-5 clear-left space-outside-up-sm">
-              <label>Gemeente</label>
+              <label class='font-md text-light xs-text-center'>Gemeente</label>
               @include('partials.select', ['name' => 'municipality',  'options' => $municipalities ])
             </div>
             <div class="col-lg-5 clear-left space-outside-up-sm">
-              <label>Geboortedatum</label>
+              <label class='font-md text-light xs-text-center'>Geboortedatum</label>
               <input type='date' name='geboortedatum' class='space-inside-xs space-inside-sides-sm
           font-sm xs-text-center height-auto border border-accent space-outside-xs datepicker' required/>
             </div>
             <div class="col-lg-7 clear-left space-outside-up-sm">
-              <label>Bericht</label>
+              <label class='font-md text-light xs-text-center'>Bericht</label>
               <textarea name='bericht' class='textarea border border-accent space-outside-xs' required></textarea>
             </div>
             <div class="col-lg-12 space-outside-sm">
