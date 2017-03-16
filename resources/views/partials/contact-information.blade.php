c26c50c99793ca6c72b72196e69f52b1ff31f22b
Eric Landheer <Eric Landheer>
2017-03-16 20:17:09 +0100

    Social media icons

--- resources/views/partials/contact-information.blade.php
+++ resources/views/partials/contact-information.blade.php
@@ -1,7 +1,14 @@
 <section class="container-fluid xs-space-inside-sides-xs space-inside-sides-xl bg-secondary no-overflow ">
 	<div class="row">
 
-		<div class="col-lg-4  space-inside-sm xs-clear-float right">
+		<div class="col-lg-6 col-md-6 col-sm-6 -col-xs-12 space-inside-sm xs-clear-float">
+			<div class="row space-inside-sides-md">
+				<a class='inline xs-space-inside-left-xs space-inside-right-xs' href='https://www.facebook.com/Mentor4You-Drenthe-599475793411930/'><img src='images/facebook.png' style='max-width:30px;' /></a></span>
+				<a class='inline xs-space-inside-left-xs'' href='https://twitter.com/m4ydrenthe'><img src='images/twitter.png' style='max-width:30px;' /></a></span>
+			</div>
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 space-inside-sm xs-clear-float right">
 			<div class="row">
 
 				<div class="col-lg-6 col-xs-12 ">
